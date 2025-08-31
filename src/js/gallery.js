d88012dcdb066ef01816311d3967d9babc11946a
SilviatovarDev <tovar.herrera.silvia@gmail.com>
2025-08-31 02:46:03 +0200

    nuevos videos

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,32 +1,52 @@
-
-//Función que recoge los datos creados en el archivo json, recorriendo los elementos y creando el elemento html con las propiedades que he definido en el archivo.
+//Función que carga la galería desde el JSON y genera HTML
 export function cargarGaleria() {
   const grid = document.getElementById("galeria-json");
   if (!grid) return;
 
-  fetch("data/gallery.json") 
+  fetch("data/gallery.json")
     .then(res => res.json())
     .then(data => {
       grid.innerHTML = data
-        .map(
-          (item) => `
-        <figure class="item" 
-          data-categoria="${item.category}" 
-          data-etiquetas="${item.tags.join(",")}" 
-          data-vistas="${item.views}" 
-          data-fecha="${item.createdAt}">
-          ${
-            item.src.type === "image"
-              ? `<img src="${item.src.webp}" alt="${item.title}" width="${item.width}" height="${item.height}" loading="lazy">`
-              : `<video autoplay muted loop playsinline
-                     poster="${item.src.poster || ""}">
-                   <source src="${item.src.mp4}" type="video/mp4">
-                 </video>`
+        .map((item) => {
+          if (item.src.type === "image") {
+            return `
+              <figure class="item"
+                data-categoria="${item.category}"
+                data-etiquetas="${item.tags.join(",")}"
+                data-vistas="${item.views}"
+                data-fecha="${item.createdAt}">
+                
+                <picture>
+                  <!--pantallas<= 768px-->
+                  <source srcset="${item.src.small}" media="(max-width: 768px)">
+                  <img 
+                    src="${item.src.large}" 
+                    alt="${item.title}" 
+                    width="${item.width}" 
+                    height="${item.height}" 
+                    loading="lazy">
+                </picture>
+
+                <figcaption>${item.title}</figcaption>
+              </figure>
+            `;
+          } else {
+            return `
+              <figure class="item"
+                data-categoria="${item.category}"
+                data-etiquetas="${item.tags.join(",")}"
+                data-vistas="${item.views}"
+                data-fecha="${item.createdAt}">
+                
+                <video autoplay muted loop playsinline poster="${item.src.poster || ""}">
+                  <source src="${item.src.mp4}" type="video/mp4">
+                </video>
+                
+                <figcaption>${item.title}</figcaption>
+              </figure>
+            `;
           }
-          <figcaption>${item.title}</figcaption>
-        </figure>
-      `
-        )
+        })
         .join("");
     })
     .catch((err) => console.error("Error cargando galería:", err));
